aa69404b997fe6e7adbb013fc3d9a03906a2a756
Reza1290 <reza.muktasib@gmail.com>
2023-11-23 23:12:11 +0700

    gg

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
-from db import connect
+from .db import connect
+from flask import request
 
 home = Blueprint('home', __name__)
 
@@ -8,14 +9,97 @@ def home_home():
     return "Hi!"
 
 
-@home.route('/keuangan')
+@home.route('/keuangan', methods=['GET'])
 def get_all_keuangan():
-    
+    connection = None
     try:
-        with connect.cursor(dictionary=True) as cursor:
+        connection = connect()
+        with connection.cursor(dictionary=True) as cursor:
             cursor.execute('SELECT * FROM keuangans')
             result = cursor.fetchall()
             return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
     finally:
-        connection.close()
-    
+        if connection:
+            connection.close()
+
+@home.route('/keuangan/<int:id>', methods=['GET'])
+def get_keuangan_by_id(id):
+    connection = None
+    try:
+        connection = connect()
+        with connection.cursor(dictionary=True) as cursor:
+            cursor.execute('SELECT * FROM keuangans WHERE id=%s', (id,))
+            result = cursor.fetchone()
+
+            if result:
+                return jsonify(result)
+            else:
+                return jsonify({'error': f'Keuangan with id {id} not found'}), 404
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        if connection:
+            connection.close()
+
+@home.route('/keuangan', methods=['POST'])
+def create_keuangan():
+    connection = None
+    try:
+        connection = connect()
+        with connection.cursor(dictionary=True) as cursor:
+            data = request.json
+
+            
+            if 'tanggal_arsip' not in data or 'pemasukan' not in data or 'pengeluaran' not in data:
+                return jsonify({'error': 'Invalid data format'}), 400
+
+            cursor.execute('INSERT INTO keuangans (tanggal_arsip, pemasukan, pengeluaran) VALUES (%s, %s, %s)',
+                           (data['tanggal_arsip'], data['pemasukan'], data['pengeluaran']))
+            connection.commit()
+            return jsonify({'message': 'Keuangan created successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        if connection:
+            connection.close()
+
+
+@home.route('/keuangan/<int:id>', methods=['PUT'])
+def update_keuangan(id):
+    connection = None
+    try:
+        connection = connect()
+        with connection.cursor(dictionary=True) as cursor:
+            data = request.json
+
+            
+            if 'tanggal_arsip' not in data or 'pemasukan' not in data or 'pengeluaran' not in data:
+                return jsonify({'error': 'Invalid data format'}), 400
+
+            cursor.execute('UPDATE keuangans SET tanggal_arsip=%s, pemasukan=%s, pengeluaran=%s WHERE id=%s',
+                           (data['tanggal_arsip'], data['pemasukan'], data['pengeluaran'], id))
+            connection.commit()
+            return jsonify({'message': f'Keuangan with id {id} updated successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        if connection:
+            connection.close()
+
+@home.route('/keuangan/<int:id>', methods=['DELETE'])
+def delete_keuangan(id):
+    connection = None
+    try:
+        connection = connect()
+        with connection.cursor(dictionary=True) as cursor:
+            cursor.execute('DELETE FROM keuangans WHERE id=%s', (id,))
+            connection.commit()
+            return jsonify({'message': f'Keuangan with id {id} deleted successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        if connection:
+            connection.close()
+            
--- controllers/db.py
+++ controllers/db.py
@@ -3,15 +3,17 @@ import os
 
 # Get MySQL container connection details from environment variables
 def connect():
-    db_host = os.environ.get('DB_HOST', 'localhost')
-    db_user = os.environ.get('DB_USER', 'username')
-    db_password = os.environ.get('DB_PASSWORD', 'password')
-    db_name = os.environ.get('DB_NAME', 'your_database_name')
+    db_host = os.environ.get('DB_HOST', '103.101.224.67')
+    db_user = os.environ.get('DB_USER', 'root')
+    db_password = os.environ.get('DB_PASSWORD', 'Password')
+    db_name = os.environ.get('DB_NAME', 'sim_rs')
+    db_port = os.environ.get('DB_PORT','13306')
     
     # Establish a connection to the MySQL container
-    connection = mysql.connector.connect(
+    return  mysql.connector.connect(
         host=db_host,
         user=db_user,
         password=db_password,
-        database=db_name
+        database=db_name,
+        port=db_port
     )
